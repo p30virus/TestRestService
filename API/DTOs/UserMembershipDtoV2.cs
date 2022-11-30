fdb637ed13a02d6ddfc7ec434be41947382f70b6
Gabriel Labarrera <p30virus@users.noreply.github.com>
2022-11-30 11:38:40 -0500

    Membership V2 DTO

---
+++ API/DTOs/UserMembershipDtoV2.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using API.Entities;
+
+namespace API.DTOs
+{
+    public class UserMembershipDtoV2
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string GivenName { get; set; }
+        [Required]
+        public string sn { get; set; }
+        [Required]
+        public string cn { get; set; }
+        [Required]
+        public string EmployeeNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int Status { get; set; }
+        [Required]
+        public List<AppGroupMembership> Membership { get; set; }
+    }
+}
